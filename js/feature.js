455b8d41111aaca67c4f2773f5b0f92c35c489eb
yorkHan <393900482@qq.com>
2018-06-14 23:47:32 +0800

    新建feature.js

---
+++ js/feature.js
@@ -0,0 +1,60 @@
+pen.onclick=function(){
+    erasering=false
+    pen.classList.add("active")
+    eraser.classList.remove("active")
+}
+eraser.onclick=function(){
+    erasering=true
+    eraser.classList.add("active")
+    pen.classList.remove("active")
+}
+clear.onclick=function(){
+    context.clearRect(0,0,draw1.clientWidth,draw1.clientHeight)
+}
+thin.onclick=function(){
+    lineWidth=5
+    thin.classList.add("active")
+    thick.classList.remove("active")
+}
+thick.onclick=function(){
+    lineWidth=10
+    thick.classList.add("active")
+    thin.classList.remove("active")
+}
+black.onclick=function(){
+    black.className="active"
+    blue.classList.remove("active")
+    yellow.classList.remove("active")
+    red.classList.remove("active")
+    context.strokeStyle="black"
+}
+red.onclick=function(){
+    red.className="active"
+    blue.classList.remove("active")
+    yellow.classList.remove("active")
+    black.classList.remove("active")
+    context.strokeStyle="red"
+}
+blue.onclick=function(){
+    blue.className="active"
+    red.classList.remove("active")
+    yellow.classList.remove("active")
+    black.classList.remove("active")
+    context.strokeStyle="blue"
+}
+yellow.onclick=function(){
+    yellow.className="active"
+    blue.classList.remove("active")
+    red.classList.remove("active")
+    black.classList.remove("active")
+    context.strokeStyle="yellow"
+}
+download.onclick=function(){
+    var url=draw1.toDataURL("image/png")
+    var a=document.createElement('a')
+    document.body.appendChild(a)
+    a.href=url
+    a.download='我的绘图'
+    a.target='_blank'
+    a.click()
+}
